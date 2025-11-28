195f36a719890812062923f05674e296a926fa79
emerss001 <emersonnevess80@gmail.com>
2025-11-28 00:15:09 -0300

    Adicionando página de agendamentos com listagem de reservas confirmadas e finalizadas

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'bookings'
+
 urlpatterns = [
+    path('<int:user_id>/', views.booking_list, name='booking_list'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Booking
 
-# Create your views here.
+def booking_list(request, user_id):
+    bookings_confirmed = Booking.objects.filter(customer_id=user_id, status='CONFIRMADO')
+    bookings_finished = Booking.objects.filter(customer_id=user_id, status='CONCLUIDO')
+
+    selected_booking = None
+    if bookings_confirmed:
+        selected_booking = bookings_confirmed[0]
+    elif bookings_finished:
+        selected_booking = bookings_finished[0]
+    else:
+        selected_booking = None
+    return render(request, 'pages/bookings/booking_list.html', {'user_id': user_id, 'bookings_confirmed': bookings_confirmed, 'bookings_finished': bookings_finished, 'selected_booking': selected_booking  })
